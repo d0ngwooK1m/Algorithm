1158af24e79e384d8613b4dffceca679d2d99b93
d0ngwook1m <dongwookim.dev@gmail.com>
2021-06-05 23:24:55 +0900

    순열

---
+++ 520_브루트포스_순열/2_이전_순열v.js
@@ -0,0 +1,34 @@
+const input = `5
+5 4 3 2 1`.split('\n');
+
+const n = Number(input[0]);
+let a = input[1].split(' ').map((x) => Number(x));
+
+const pastPermutation = function (a) {
+    let i = n-1;
+    while (i > 0 && a[i-1] <= a[i]) i -= 1;
+    if (i <= 0) return false;
+    
+    let j = n-1;
+    while (a[j] >= a[i-1]) j -= 1;
+    let temp = a[j];
+    a[j] = a[i-1];
+    a[i-1] = temp;
+
+    j=n-1;
+    while (i < j) {
+        let temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        i += 1;
+        j += 1;
+    }
+
+    return true;
+}
+
+if (pastPermutation(a)) {
+    console.log(a.join(' '));
+} else {
+    console.log(-1);
+}
